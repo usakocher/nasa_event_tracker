cd41bc5d075968960b15de7d6cd6159f70f47ba4
usakocher <adammkocher@gmail.com>
2021-09-23 19:23:56 -0400

    Storm Paths

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,10 +1,41 @@
+import { useState, useEffect } from "react";
+
+const API_KEY = process.env.REACT_APP_NEWS_API_KEY
+
 const LocationInfoBox = ({ info }) => {
-    return (
+    const getData = async (title) => {
+        const today = new Date();
+        const month = today.getMonth() + 1;
+        const date = today.getDate();
+        const year = today.getFullYear();
+        title = title.replace(/[^\w\s]|_/g, "")
+            .replace(/\s+/g, " ");
+        const listTitle = title.split(' ');
+        const searchItems = listTitle.join('&');
+        console.log(searchItems)
+        const result = await fetch(`https://newsapi.org/v2/everything?q=${searchItems}&from=${year}-${month}-${date}&to=${year}-${month}-${date}sortBy=relevancy&apiKey=${API_KEY}`);
+        const data = await result.json();
+        console.log(data)
+        return [data.articles[0].title, data.articles[0].url, data.articles[1].title, data.articles[1].url]
+    }  
+
+    
+
+    const [news, setNews] = useState([])
+
+    // { getData().then(newsHook => setNews(newsHook)) }
+    useEffect(()=>{
+            getData(info.title).then(newsHook => setNews(newsHook))
+        }, []) // <-- empty dependency array
+    
+    return (        
         <div className="location-info">
             <h2>Event Location Info</h2>
             <ul>
-                <li>ID: <strong>{ info.id }</strong></li>
                 <li>Title: <strong>{ info.title }</strong></li>
+                <h3>News</h3>
+                <li><a href={ news[1] } target='blank'>{ news[0] }</a></li>
+                <li><a href={ news[3] } target='blank'>{ news[2] }</a></li>
             </ul>
         </div>
     )
--- src/components/Map.js
+++ src/components/Map.js
@@ -9,13 +9,17 @@ const API_KEY = process.env.REACT_APP_API_KEY
 const Map = ({ eventData, center, zoom }) => {
     const [locationInfo, setLocationInfo] = useState(null);
 
+
     const markers = eventData.map(ev => {
         if(ev.categories[0].id === 8) {
             return <FireMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
-                                        onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}/>
+                                        onClick={() => setLocationInfo({ title: ev.title })}/>
         }else if(ev.categories[0].id === 10){
-            return <StormMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
-                                        onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}/>
+            let path = [];
+            for (let i=0; i< ev.geometries.length; i+=2){
+                path.push(<StormMarker lat={ev.geometries[i].coordinates[1]} lng={ev.geometries[i].coordinates[0]}
+                                        onClick={() => setLocationInfo({ title: ev.title })}/>)}
+            return path
         }
         return null
     })
@@ -40,7 +44,7 @@ Map.defaultProps = {
         lat: 42.3265,
         lng: -122.8756
     },
-    zoom: 6
+    zoom: 4.5
 }
 
 export default Map
